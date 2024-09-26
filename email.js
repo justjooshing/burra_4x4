4ae67e1d47ec6be8d181a448d01a4395258fc84e
Josh <joshua.l.mertens@gmail.com>
2024-09-26 15:47:05 +1000

    chore: update emailJS handler with new serviceID
    chore: separate emailjs logic from other logic

---
+++ email.js
@@ -0,0 +1,39 @@
+// Initialise EmailJS
+emailjs.init("GzVIhjxHJvn0KjhhO");
+
+const contactUsForm = document.getElementById("contact_us_form");
+const contactSubmitButton = document.getElementById("contact_submit_button");
+const formSubmissionError = document.getElementById("form_submission_error");
+
+const handleEmailSubmission = async () => {
+  try {
+    contactSubmitButton.innerText = "Sending...";
+    await emailjs.sendForm(
+      "service_burracar4x4",
+      "template_fms1jmt",
+      "contact_us_form",
+    );
+    toggleElementVisibility("thankyou");
+    contactSubmitButton.innerText = "Sent";
+    contactUsForm.reset();
+  } catch (err) {
+    console.error(err);
+  } finally {
+    contactSubmitButton.innerText = "Submit";
+  }
+};
+
+contactUsForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  // Prevent double submission
+  if (contactSubmitButton.innerText === "Sending...") return;
+
+  const honeypot = document.getElementById("honeypot").value;
+  // Check if the honeypot field is empty
+  if (honeypot) {
+    // Honeypot filled; prevent form submission
+    alert("Spam detected! Your submission cannot be processed.");
+  } else {
+    handleEmailSubmission();
+  }
+});
--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// Initialise EMAILJS
-emailjs.init("GzVIhjxHJvn0KjhhO");
-
 const elementTriggerMap = {
   call_now_button: [{ element: "phone_number" }],
   map_button: [{ element: "map_window" }],
@@ -43,45 +40,4 @@ Object.entries(elementTriggerMap).forEach(([id, elements]) => {
       toggleElementVisibility(element, directive),
     );
   });
-});
-
-const contactUsForm = document.getElementById("contact_us_form");
-const contactSubmitButton = document.getElementById("contact_submit_button");
-const formSubmissionError = document.getElementById("form_submission_error");
-
-contactUsForm.addEventListener("submit", async (e) => {
-  e.preventDefault();
-  // Prevent double submission
-  if (contactSubmitButton.innerText === "Sending...") return;
-
-  const honeypot = document.getElementById("honeypot").value;
-  // Check if the honeypot field is empty
-  if (honeypot) {
-    // Honeypot filled; prevent form submission
-    alert("Spam detected! Your submission cannot be processed.");
-  } else {
-    try {
-      contactSubmitButton.innerText = "Sending...";
-      // update to sending?
-      await emailjs.sendForm(
-        "service_bmmdnnf",
-        "template_fms1jmt",
-        "contact_us_form",
-      );
-      // update to sent
-      contactSubmitButton.innerText = "Sent";
-      toggleElementVisibility("thankyou");
-      // set timeout
-      // update to submit
-      await (async () =>
-        setTimeout(() => {
-          // reset form
-          contactUsForm.reset();
-        }))();
-    } catch (err) {
-      console.error(err);
-    } finally {
-      contactSubmitButton.innerText = "Submit";
-    }
-  }
 });
